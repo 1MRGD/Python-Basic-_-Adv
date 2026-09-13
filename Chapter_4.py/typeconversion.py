3a650dfe4a597b48fa8e284dffa9235310d74f78
Ashish Ranjan Samal <153530109+1MRGD@users.noreply.github.com>
2026-09-13 09:28:49 +0000

    Done Type Conversion and Slicing part.

--- Chapter_4.py/typeconversion.py
+++ Chapter_4.py/typeconversion.py
@@ -3,8 +3,8 @@
 
 # Each Character in a string with its own unicode number . That's why string use more memory than the int 
 
-# a = "COLLAGE"
-# print(a[0])
-# print(a[-1])
+a = "COLLAGE"
+print(a[0])
+print(a[-1])
 
 # string indexing = its generally two type Negative 
