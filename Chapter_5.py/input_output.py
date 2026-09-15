d74837d70048eb1a18ff06f2a11878f19c66214a
Ashish Ranjan Samal <153530109+1MRGD@users.noreply.github.com>
2026-09-15 17:23:10 +0000

    I have tried and learned about the input function in python.

---
+++ Chapter_5.py/input_output.py
@@ -0,0 +1,17 @@
+# name="GD"
+# age = 24
+
+# print("Hii my name is",name,"and my age is",age)
+# print(f"Hii my name is {name} and my age is {age}") //  Here f is there for formated sentense
+
+
+# age = input("What is your age :-")
+# print(f"Hello your age is {age}")
+
+# Input () always return a string 
+#-> If You need a number , Convert it manually with iny() or float()
+
+# name = input("What Is Your Name? ")
+# age = int(input("How old are you? "))
+
+# print(f"Hello My Name is {name} and age is {age}")
